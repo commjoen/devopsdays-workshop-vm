4e35b5087e99d51fd6e4fcb05e9396641363a570
Arjan Molenaar <gaphor@gmail.com>
2016-09-25 11:37:09 +0200

    Fix default IntelliJ profile and plugins for Arjan

--- scripts/provision-intellij.sh
+++ scripts/provision-intellij.sh
@@ -1,11 +1,15 @@
 
+set -e
+
 IDEA_ARCHIVE=ideaIC-2016.2.4.tar.gz
 IDEA_VERSION=idea-IC-162.2032.8
-cd /tmp
-wget https://download.jetbrains.com/idea/${IDEA_ARCHIVE}
+
 
 mkdir -p /opt
+wget -O "/tmp/${IDEA_ARCHIVE}" "https://download.jetbrains.com/idea/${IDEA_ARCHIVE}"
+
 tar zxf ${IDEA_ARCHIVE} -C /opt
+rm "/tmp/${IDEA_ARCHIVE}"
 
 cat > /usr/share/applications/idea.desktop << EOF
 [Desktop Entry]                                                                 
@@ -19,3 +23,18 @@ StartupNotify=true
 Type=Application
 EOF
 
+echo "Provision user profile..."
+
+cp -r /vagrant/intellij/dotIdeaIC2016.2 ~vagrant/.IdeaIC2016.2
+
+echo "Installing plugins..."
+
+mkdir -p ~vagrant/.IdeaIC2016.2/system/plugins
+cd ~vagrant/.IdeaIC2016.2/system/plugins
+wget -O idea-fitnesse.zip "https://plugins.jetbrains.com/plugin/download?pr=idea&updateId=27503"
+unzip idea-fitnesse.zip
+wget -O cucumber-java.zip "https://plugins.jetbrains.com/plugin/download?pr=idea&updateId=25773"
+unzip cucumber-java.zip
+
+
+chown -R vagrant:vagrant ~vagrant/.IdeaIC2016.2
