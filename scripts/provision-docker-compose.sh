31eb417c2005420b84c9504f70db27b393ed5a22
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-09-28 16:31:00 +0100

    Add missing sudo to Docker Compose installation

--- scripts/provision-docker-compose.sh
+++ scripts/provision-docker-compose.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-curl -s -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -s -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
