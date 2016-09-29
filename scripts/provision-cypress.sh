a6680d4417beb74747be924995f9af1541fdabd9
Randall Kent <randall@cypress.io>
2016-09-29 17:10:16 -0400

    Cypress headless dependencies & more example repos!

--- scripts/provision-cypress.sh
+++ scripts/provision-cypress.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Provisioning Cypress Dependencies..."
+sudo apt-get install xvfb
+
 echo "Provisioning Cypress..."
+mkdir cypress-workshop
+cd cypress-workshop
 sudo npm install -g cypress-cli
 cypress install
 
 echo "Provisioning cypress-example-todomvc..."
-mkdir cypress-workshop
-cd cypress-workshop
 git clone https://github.com/cypress-io/cypress-example-todomvc.git
 cd cypress-example-todomvc
+npm install
+
+echo "Provisioning cypress-example-phonecat..."
+cd ~/cypress-workshop
+git clone https://github.com/cypress-io/cypress-example-phonecat.git
+cd cypress-example-phonecat
+npm install
+
+echo "Provisioning cypress-example-kitchensink..."
+cd ~/cypress-workshop
+git clone https://github.com/cypress-io/cypress-example-kitchensink.git
+cd cypress-example-kitchensink
 npm install
