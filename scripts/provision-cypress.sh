4e543d48f6b4e405cac95ee0707cda250d7d48b2
Jeroen Willemsen <jwillemsen@xebia.com>
2017-06-02 15:52:38 +0200

    updated zap and removed cypress workshop

--- scripts/provision-cypress.sh
+++ scripts/provision-cypress.sh
@@ -11,19 +11,19 @@ cd cypress-workshop
 sudo npm install -g cypress-cli
 cypress install
 
-echo "Provisioning cypress-example-todomvc..."
-git clone -b workshop https://github.com/cypress-io/cypress-example-todomvc.git
-cd cypress-example-todomvc
-npm install
-
-echo "Provisioning cypress-example-phonecat..."
-cd ~/conference-day/cypress-workshop
-git clone -b workshop https://github.com/cypress-io/cypress-example-phonecat.git
-cd cypress-example-phonecat
-npm install
-
-echo "Provisioning cypress-example-kitchensink..."
-cd ~/conference-day/cypress-workshop
-git clone https://github.com/cypress-io/cypress-example-kitchensink.git
-cd cypress-example-kitchensink
-npm install
+# echo "Provisioning cypress-example-todomvc..."
+# git clone -b workshop https://github.com/cypress-io/cypress-example-todomvc.git
+# cd cypress-example-todomvc
+# npm install
+#
+# echo "Provisioning cypress-example-phonecat..."
+# cd ~/conference-day/cypress-workshop
+# git clone -b workshop https://github.com/cypress-io/cypress-example-phonecat.git
+# cd cypress-example-phonecat
+# npm install
+#
+# echo "Provisioning cypress-example-kitchensink..."
+# cd ~/conference-day/cypress-workshop
+# git clone https://github.com/cypress-io/cypress-example-kitchensink.git
+# cd cypress-example-kitchensink
+# npm install
--- scripts/provision-zap.sh
+++ scripts/provision-zap.sh
@@ -5,7 +5,7 @@ echo "Provisioning ZAP..."
 cd Documents
 mkdir tools
 cd tools
-wget https://github.com/zaproxy/zaproxy/releases/download/2.5.0/ZAP_2.5.0_Linux.tar.gz
-tar xvfx ZAP_2.5.0_Linux.tar.gz
-rm -rf ZAP_2.5.0_Linux.tar.gz
-echo "export PATH=\"/home/vagrant/Documents/tools/ZAP_2.5.0;$PATH\"" >> /home/vagrant/.zshrc
+wget https://github.com/zaproxy/zaproxy/releases/download/2.6.0/ZAP_2.6.0_Linux.tar.gz
+tar xvfx ZAP_2.6.0_Linux.tar.gz
+rm -rf ZAP_2.6.0_Linux.tar.gz
+echo "export PATH=\"/home/vagrant/Documents/tools/ZAP_2.6.0;$PATH\"" >> /home/vagrant/.zshrc
