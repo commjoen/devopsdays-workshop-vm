86e4315ebd5aa6c496515ca6f39d5ad82aba6cbc
Nanne Baars <Nanne Baars>
2017-06-02 15:11:42 +0200

    Upgrading to a new JDK version

--- scripts/provision-jdk.sh
+++ scripts/provision-jdk.sh
@@ -5,21 +5,21 @@ set -e
 echo "Provisioning Java JDK..."
 mkdir -p /home/vagrant/java
 cd /home/vagrant/java
-test -f /tmp/jdk-8-linux-x64.tar.gz || curl -q -L --cookie "oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-x64.tar.gz -o /tmp/jdk-8-linux-x64.tar.gz
+test -f /tmp/jdk-8-linux-x64.tar.gz || curl -q -L --cookie "oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -o /tmp/jdk-8-linux-x64.tar.gz
 
 sudo mkdir -p /usr/lib/jvm
 
 sudo tar zxf /tmp/jdk-8-linux-x64.tar.gz -C /usr/lib/jvm
 
 # register Java
-sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_101/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_101/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0_101/bin/javaws" 1
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_131/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_131/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0_131/bin/javaws" 1
 
 sudo chmod a+x /usr/bin/java
 sudo chmod a+x /usr/bin/javac
 sudo chmod a+x /usr/bin/javaws
-sudo chown -R root:root /usr/lib/jvm/jdk1.8.0_101
+sudo chown -R root:root /usr/lib/jvm/jdk1.8.0_131
 
 #sudo echo -e "export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_101" >> /etc/environment
-echo "export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_101" >> /home/vagrant/.zshrc
+echo "export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_131" >> /home/vagrant/.zshrc
