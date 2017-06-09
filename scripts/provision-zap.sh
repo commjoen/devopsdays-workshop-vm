bcad8a90d10b2c8585b40b107c8d0ff08cbc5335
Jeroen Willemsen <jwillemsen@xebia.com>
2017-06-09 10:02:10 +0200

    re-fix zap integration

--- scripts/provision-zap.sh
+++ scripts/provision-zap.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "Provisioning ZAP..."
-cd Documents
-mkdir tools
+cd conference-day
+cd security-testing
 cd tools
-wget –quiet https://github.com/zaproxy/zaproxy/releases/download/2.6.0/ZAP_2.6.0_Linux.tar.gz
+wget –quiet https://github.com/zaproxy/zaproxy/releases/download/2.6.0/ZAP_2.6.0_Linux.tar.gz || exit 0
 tar xvfx ZAP_2.6.0_Linux.tar.gz
 rm -rf ZAP_2.6.0_Linux.tar.gz
-echo "export PATH=\"/home/vagrant/Documents/tools/ZAP_2.6.0;$PATH\"" >> /home/vagrant/.zshrc
+echo "export PATH=\"/home/vagrant/conference-day/security-testing/tools/ZAP_2.6.0;$PATH\"" >> /home/vagrant/.zshrc
